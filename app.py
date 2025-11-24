3fb95160f4395915df7824ae4916849f38d516fe
Diana Alexandre <dianaalexandre@usf.edu>
2025-11-23 23:05:19 -0500

    Add Project to folder. Include app.py with welcome message.

---
+++ app.py
@@ -0,0 +1,14 @@
+# Welcome
+print("Welcome to my Python Program!")
+# Ask user for numeric input
+books = input("How many books did you read today?")
+# Basic error handling for non-numeric values
+try:
+    books = float(books)
+except ValueError:
+    print("Please enter a valid number.")
+    exit()
+# Perform Calculation
+weekly_books = books * 7
+# Display Book Count
+print(f"You are on track to read {weekly_books} books this week.")
